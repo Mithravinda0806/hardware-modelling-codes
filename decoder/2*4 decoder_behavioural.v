3be46fa4e2fe09a01891d558d6a0d99f282a3113
Mithravinda0806 <168867151+Mithravinda0806@users.noreply.github.com>
2026-03-02 20:11:16 +0530

    2_4 decoder_behavioural.v

---
+++ decoder/2*4 decoder_behavioural.v
@@ -0,0 +1,14 @@
+module decode_2_4_bh(Y,I,EN);
+  input  [1:0]I;
+  input EN;
+  output [3:0]Y;
+  always @( *) begin
+    case({EN,I})
+      3'b100:Y=4'b0001;
+      3'b101:Y=4'b0010;
+      3'b110:Y=4'b0100;
+      3'b111:Y=4'b1000;
+      default: Y = 4'b0000;
+    endcase
+  end
+endcase
